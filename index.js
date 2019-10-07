7b0c8fd7f141e4a908b9a044e3a3f6a5b45580ef
zSteinO34 <zstein1234@gmail.com>
2019-10-07 13:25:50 -0500

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,18 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(string) {
+  let time = parseInt(string);
+  if(time < 12) {
+    return 'Good Morning';
+  } else if(time >=12  && time < 17) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
+
+function displayMessage(string) {
+  let m = document.getElementById("greeting");
+  m.innerText = string;
+}
